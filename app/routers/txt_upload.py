de4b940e9448bbc352f880eae112d6b5e68a03db
Rohit Kale <rohit.kale@themathcompany.com>
2022-07-13 15:12:51 +0530

    Duplicate username and files

--- app/routers/txt_upload.py
+++ app/routers/txt_upload.py
@@ -1,5 +1,5 @@
-from http.client import HTTPException
-from fastapi import APIRouter, File, Security, UploadFile,Depends, status
+from base64 import decode
+from fastapi import APIRouter, File, Security, UploadFile,Depends, HTTPException, status
 from requests import Session
 import models
 from db_database import get_db
@@ -30,13 +30,21 @@ def upload(file: UploadFile = File(), db : Session = Depends(get_db),credentials
     token = credentials.credentials
     decoded = auth_handler.decode_token(token)
     user = db.query(models.User).filter(models.User.username == decoded).first()
+    if decoded == user.username:
+        file_details = models.file_data(filename = file.filename,length = txt_len(file.filename),user_id = user.uuid)
+        all_files = []
+        for i in db.query(models.file_data).filter(models.file_data.user_id== file_details.user_id).all():
+            all_files.append(i.filename)
+        if file_details.filename in all_files:
+            return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='file already exists')
+        else:
+            db.add(file_details)
+            db.commit()
+            db.refresh(file_details)
+            return file_details
+    else:
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='Invalid token')
     
-    file_details = models.file_data(filename = file.filename,length = txt_len(file.filename),user_id = user.uuid)
-    db.add(file_details)
-    db.commit()
-    db.refresh(file_details)
-    
-    return file_details
     
     
 
--- app/routers/user.py
+++ app/routers/user.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter,Depends, HTTPException
+from fastapi import APIRouter,Depends, HTTPException,status
 from requests import Session
 import schemas,models
 from db_database import get_db
@@ -17,11 +17,17 @@ auth_handler = Auth()
 def create_User(request: schemas.User, db : Session = Depends(get_db)):
     new_uuid = str(uuid.uuid4())
     new_user = models.User(username= request.username,uuid = new_uuid)
-    db.add(new_user)
-    db.commit()
-    db.refresh(new_user)
-    access_token = auth_handler.encode_token( new_user.username)
-    return {"access_token": access_token, "uuid": new_uuid}
+    all_users = []
+    for i in db.query(models.User).all():
+        all_users.append(i.username)
+    if new_user.username in all_users:
+        return HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='user exists')
+    else:
+        db.add(new_user)
+        db.commit()
+        db.refresh(new_user)
+        access_token = auth_handler.encode_token( new_user.username)
+        return {"access_token": access_token, "uuid": new_uuid}
 
 
 
